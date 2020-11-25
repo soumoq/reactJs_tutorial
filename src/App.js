3a5aa605d2963231aaa97fd3c625d6120fad634f
root <soumojyotiganguly270@gmail.com>
2020-11-26 00:07:47 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { Greet } from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello';
+import Message from './components/Message';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Greet name = "Ram" type = "Student">
-        <p>
-          Welcome to hell
-        </p>
-      </Greet>
-      <Greet name = "Sam" type = "Hod">
-        <button>
-          Action
-        </button>
-      </Greet>
-      <Greet name = "Jdu" type = "Teacher"/>
-
-      <Welcome name = "DC"/>
-      <Welcome name = "Marvel"/>
-
-
+     <Message />
     </div>
   );
 }
